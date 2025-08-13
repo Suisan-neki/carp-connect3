9d8fa107f9b2c6fcf98c958ebfeef37d4f9acbfa
suisan <b241651@hiroshima-u.ac.jp>
2025-08-13 14:56:29 +0900

    deploy2

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import IndexPage from './carp-connect-frontend/pages/index.jsx';
+import Layout from './carp-connect-frontend/components/layout/Layout.jsx';
+import './carp-connect-frontend/src/index.css';
 import './carp-connect-frontend/lib/aws/config.js'; // Amplifyの設定をインポート
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <IndexPage />
+    <Layout>
+      <IndexPage />
+    </Layout>
   </React.StrictMode>,
 );
